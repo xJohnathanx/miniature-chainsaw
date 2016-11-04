8ef740413d34b4c6561946cadfa13e602003e971
xJohnathanx <ashfordchalee5@gmail.com>
2016-11-03 20:57:55 -0400

    Update database.php

--- application/database.php
+++ application/database.php
@@ -1,10 +1,10 @@
 <?php
 defined('DIRECT_ACCESS') || die('Direct access is not allowed');
 
-$dbh_hostname = $database_config['hostname'];
-$dbh_username = $database_config['username'];
-$dbh_password = $database_config['password'];
-$dbh_database = $database_config['database'];
+$dbh_hostname = $database_config['Localhost'];
+$dbh_username = $database_config['Administrator'];
+$dbh_password = $database_config['clubbohotel'];
+$dbh_database = $database_config['Localhost'];
 $dbh_port = $database_config['port'];
 
 $mysql_port = '';
